8f3393167ffe4ece43e5afb523727796b2c9caf9
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-03-01 14:29:36 +0000

    Add files via upload
    geo location, APIs

---
+++ map.js
@@ -0,0 +1,17 @@
+const map = L.map('myMap');
+map.setView({lon: 0, lat: 0}, 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
+
+L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+
+const button = document.querySelector('#find-me');
+
+function doZoom() {
+    map.setZoom(5);
+    }
+
+    button.addEventListener('click', doZoom);
